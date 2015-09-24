2bd7e6b66afbbf43b87b98ab3e72d3af59075d62
Michiel Min <michiel.min@gmail.com>
2015-09-24 14:24:31 +0200

    Stability improvement on the rotate stokes subroutine

--- Radiation.f
+++ Radiation.f
@@ -523,7 +523,7 @@ c------------------------------------------------------------------------
 	use GlobalSetup
 	IMPLICIT NONE
 	type(photon) phot
-	real*8 x,y,z,cost,sint,Q,U,P,P2,cos2t,sin2t,sPP2
+	real*8 x,y,z,cost,sint,Q,U,P,cos2t,sin2t,een
 	
 	P=(phot%sU/phot%sI)**2+(phot%sQ/phot%sI)**2
 
@@ -536,15 +536,16 @@ c------------------------------------------------------------------------
 	
 	cos2t=cost**2-sint**2
 	sin2t=2d0*sint*cost
+
+	een=sqrt(sin2t**2+cos2t**2)
+	sin2t=sin2t/een
+	cos2t=cos2t/een
 	
 	Q=phot%sQ*cos2t+phot%sU*sin2t
 	U=-phot%sQ*sin2t+phot%sU*cos2t
 
-	P2=(U/phot%sI)**2+(Q/phot%sI)**2
-
-	sPP2=sqrt(P/P2)
-	phot%sU=sPP2*U
-	phot%sQ=sPP2*Q
+	phot%sU=U
+	phot%sQ=Q
 	
 	return
 	end
