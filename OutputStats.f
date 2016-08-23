b6ffc9a14740b87a284fd5d7a83384df8c85987e
Michiel Min <michiel.min@gmail.com>
2016-08-23 17:01:06 +0200

    Added heightR files for all zones

--- OutputStats.f
+++ OutputStats.f
@@ -2,7 +2,8 @@
 	use GlobalSetup
 	use Constants
 	IMPLICIT NONE
-	real*8 lam0,d,GetKext,radtau,Mtot
+	real*8 lam0,d,GetKext,radtau,Mtot,tau
+	real*8,allocatable :: rad(:)
 	integer ilam,i,izone,ir,ip,it,is
 
 	lam0=0.55
@@ -33,6 +34,27 @@
 		enddo
 		call output("Optical depth through zone " // trim(int2string(izone,"(i4)")) // 
      &						":" // trim(dbl2string(Zone(izone)%tau_V,"(e14.4)")))
+
+		open(unit=20,file=trim(outputdir) // "heightR" // trim(int2string(izone,'(i0.4)')) // ".dat",RECL=6000)
+		allocate(rad(Zone(izone)%np))
+		do it=1,Zone(izone)%nt
+			do ip=1,Zone(izone)%np
+				radtau=0d0
+				rad(ip)=0d0
+				do ir=1,Zone(izone)%nr
+					tau=GetKext(ilam,Zone(izone)%C(ir,it,ip))*
+     &					(Zone(izone)%R(ir+1)-Zone(izone)%R(ir))
+					if(radtau+tau.gt.1d0) then
+						rad(ip)=Zone(izone)%R(ir)+(Zone(izone)%R(ir+1)-Zone(izone)%R(ir))*(1d0-radtau)/tau
+						exit
+					endif
+					radtau=radtau+tau
+				enddo
+			enddo
+			write(20,*) (rad(ip)*sin(Zone(izone)%theta(it))/AU,rad(ip)*cos(Zone(izone)%theta(it))/AU,ip=1,min(Zone(izone)%np,125))
+		enddo
+		deallocate(rad)
+		close(unit=20)
 	enddo
 
 	if(adjustAv) then
